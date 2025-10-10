ade40d78409b87dd6895ec9768698dfbbeb1bed6
iskream23 <mikamailpro@gmail.com>
2025-10-10 17:18:27 +0300

    Added game map

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -15,6 +15,7 @@ export class Game extends Scene
     preload()
     {
         this.load.spritesheet('player', 'assets/spritesheet.png',{frameWidth: 16, frameHeight: 16});
+        this.load.image('tiles', 'assets/spritesheet.png');
     }
 
     create ()
@@ -25,7 +26,32 @@ export class Game extends Scene
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0.5);
 
-        this.player = new Player(this, 50, 50);
+        const level = [
+            [ 85, 70, 70, 70, 70, 70, 70, 70, 70, 85 ],
+            [ 85, 1, 1, 1, 1, 1, 1, 1, 1, 85 ],
+            [ 85, 1, 1, 1, 1, 1, 1, 1, 1, 85 ],
+            [ 85, 1, 1, 1, 1, 1, 1, 1, 1, 85 ],
+            [ 85, 1, 1, 1, 1, 1, 1, 1, 1, 85 ],
+            [ 85, 1, 1, 1, 1, 1, 1, 1, 1, 85 ],
+            [ 85, 1, 1, 1, 1, 1, 1, 1, 1, 85 ],
+            [ 85, 1, 1, 1, 1, 1, 1, 1, 1, 85 ],
+            [ 85, 1, 1, 1, 1, 1, 1, 1, 1, 85 ],
+            [ 70, 70, 70, 70, 70, 70, 70, 70, 70, 70 ],
+        ]
+
+        const map = this.make.tilemap({data: level, tileWidth: 16, tileHeight: 16});
+        const tiles = map.addTilesetImage('tiles');
+
+        if (!tiles) {
+            console.error('Failed to load tileset!');
+            return;
+        }
+
+        const layer = map.createLayer(0, tiles, 0, 0);
+
+        const playerX = (1 * 16) + 8;
+        const playerY = (1 * 16) + 8;
+        this.player = new Player(this, playerX, playerY);
 
         this.input.once('pointerdown', () => {
             this.scene.start('GameOver');
