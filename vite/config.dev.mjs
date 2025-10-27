392970deb99a89f75fbf1155a5d75db7afdb52b7
iskream23 <mikamailpro@gmail.com>
2025-10-27 20:53:08 +0300

    Fixed Netlify deployment

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,17 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-    base: './',
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
-        },
+  root: resolve(__dirname, ".."),
+  base: "./",
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+    alias: {
+      "@": resolve(__dirname, "../src"),
     },
-    server: {
-        port: 8080
-    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ["phaser"],
+        },
+      },
+    },
+  },
+  server: {
+    port: 8080,
+  },
 });
--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,47 +1,57 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const phasermsg = () => {
-    return {
-        name: 'phasermsg',
-        buildStart() {
-            process.stdout.write(`Building for production...\n`);
-        },
-        buildEnd() {
-            const line = "---------------------------------------------------------";
-            const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
-            process.stdout.write(`${line}\n${msg}\n${line}\n`);
-            
-            process.stdout.write(`✨ Done ✨\n`);
-        }
-    }
-}   
+  return {
+    name: "phasermsg",
+    buildStart() {
+      process.stdout.write(`Building for production...\n`);
+    },
+    buildEnd() {
+      const line = "---------------------------------------------------------";
+      const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
+      process.stdout.write(`${line}\n${msg}\n${line}\n`);
+
+      process.stdout.write(`✨ Done ✨\n`);
+    },
+  };
+};
 
 export default defineConfig({
-    base: './',
-    logLevel: 'warning',
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
+  root: resolve(__dirname, ".."),
+  base: "./",
+  logLevel: "warning",
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+    alias: {
+      "@": resolve(__dirname, "../src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ["phaser"],
         },
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                passes: 2
-            },
-            mangle: true,
-            format: {
-                comments: false
-            }
-        }
+      },
     },
-    server: {
-        port: 8080
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        passes: 2,
+      },
+      mangle: true,
+      format: {
+        comments: false,
+      },
     },
-    plugins: [
-        phasermsg()
-    ]
+  },
+  server: {
+    port: 8080,
+  },
+  plugins: [phasermsg()],
 });
